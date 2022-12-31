e0fe744fad3f9db301c29f1958bb15717c77cbf9
Luka Agic <lukagic@gmail.com>
2022-12-31 21:04:31 +0100

    Declare variables

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+declare (strict_types = 1);
+
+require 'includes/functions.php';
+
+$user =
+    [
+    'name' => '',
+    'age' => '',
+    'terms' => '',
+];
+
+$errors =
+    [
+    'name' => '',
+    'age' => '',
+    'terms' => '',
+];
+
+$message = '';
