956d5e8140c3dd01a95bc20ba05e457f85c9efad
Luka Agic <lukagic@gmail.com>
2022-12-31 21:14:08 +0100

    Set conditionals

--- index.php
+++ index.php
@@ -19,3 +19,19 @@ $errors =
 ];
 
 $message = '';
+if ($_SERVER['REQUEST-METHOD'] == 'POST') {
+    $user['name'] = $_POST['name'];
+    $user['age'] = $_POST['age'];
+    $user['terms'] = (isset($_POST['terms']) and $_POST['terms'] == true) ? true : false;
+
+    $errors['name'] = is_text($user['name'], 2, 20) ? '' : 'Your username must be between 2 and 20 characters';
+    $errors['age'] = is_number($user['age'], 18, 65) ? '' : 'Your age must be between 18 and 65 characters';
+    $errors['terms'] = $user['terms'] ? '' : 'You must agree to the terms and conditions';
+
+    $invalid = implode($errors);
+    if ($invalid) {
+        $message = 'Please correct the following errors:';
+    } else {
+        $message = 'Your data was valid';
+    }
+}
