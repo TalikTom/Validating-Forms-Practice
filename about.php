c00438fdb1dfc50ec9b98d6eede931590df0ccb1
Luka Agic <lukagic@gmail.com>
2023-01-02 23:05:48 +0100

    Set validation filters

--- about.php
+++ about.php
@@ -3,3 +3,13 @@
 $user = ['name' => '', 'age' => '', 'terms' => '',];
 $errors = ['name' => '', 'age' => '', 'terms' => '',];
 $user = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $validation_filters['name']['filter'] = FILTER_VALIDATE_REGEXP;
+    $validation_filters['name']['options']['regexp'] = '/^[A-z]{2,10}$/';
+    $validation_filters['age']['filter'] = FILTER_VALIDATE_INT;
+    $validation_filters['age']['options']['min_range'] = 16;
+    $validation_filters['age']['options']['max_range'] = 65;
+    $validation_filters['terms'] = FILTER_VALIDATE_BOOLEAN;
+    
+}
