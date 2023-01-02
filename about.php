28d0f4c4774c8f07abac4747b2170a37b00de003
Luka Agic <lukagic@gmail.com>
2023-01-02 23:10:01 +0100

    Set error conditionals and messages

--- about.php
+++ about.php
@@ -11,5 +11,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $validation_filters['age']['options']['min_range'] = 16;
     $validation_filters['age']['options']['max_range'] = 65;
     $validation_filters['terms'] = FILTER_VALIDATE_BOOLEAN;
-    
+
+    $user = filter_input_array(INPUT_POST, $validation_filters); //validate data
+
+    //error messages
+    $errors['name'] = $user['name'] ? '' : 'Your name must be between 2 and 10 letters using characters A-z';
+    $errors['age'] = $user['age'] ? '' : 'Your age must be between 16 and 65';
+    $errors['terms'] = $user['terms'] ? '' : 'You must agree to terms and conditions';
+    $invalid = implode($errors);
+
+    if ($invalid) {
+        $message = 'Please correct the following errors';
+    } else {
+        $message = 'Thank you';
+    }
 }
