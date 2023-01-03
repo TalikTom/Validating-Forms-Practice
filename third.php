e534476c6f7e8b927a2a35382d3e5adf03d9f4f1
Luka Agic <lukagic@gmail.com>
2023-01-03 23:22:09 +0100

    Practice checking file has been uploaded

--- third.php
+++ third.php
@@ -1,5 +1,23 @@
-<form action="third.php" method="post" enctype="multipar/form-data">
+<?php
+$message = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $message = '<b>File:</b> ' . $_FILES['image']['name'] . '<br>';
+    $message .= '<b>File:</b> ' . $_FILES['image']['size'] . '<br>';
+} else {
+    $message = 'Nope';
+}
+
+?>
+<?= $message ?>
+<form action="third.php" method="post" enctype="multipart/form-data">
     <label for="image">Upload file:</label>
     <input type="file" name="image" accept="image/*"><br>
     <input type="submit" value="Upload">
 </form>
+
+<?= $_FILES['image']['name']; ?>
+<?= $_FILES['image']['tmp_name']; ?>
+<?= $_FILES['image']['size']; ?>
+<?= $_FILES['image']['type']; ?>
+<?= $_FILES['image']['error']; ?>
