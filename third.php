b2059858e65a5e3aae44100d30efd1187eb339d4
Luka Agic <lukagic@gmail.com>
2023-01-03 23:12:06 +0100

    Add form for uploading files from a browser

---
+++ third.php
@@ -0,0 +1,5 @@
+<form action="third.php" method="post" enctype="multipar/form-data">
+    <label for="image">Upload file:</label>
+    <input type="file" name="image" accept="image/*"><br>
+    <input type="submit" value="Upload">
+</form>
