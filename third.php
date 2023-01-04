e597ef98dbbaf34dfffa55c15fe55bf49b6d909e
Luka Agic <lukagic@gmail.com>
2023-01-04 19:10:02 +0100

    Initialize variables, validating file uploads

--- third.php
+++ third.php
@@ -2,16 +2,28 @@
 $message = '';
 
 $moved = '';
+$basename = pathinfo($moved, PATHINFO_FILENAME);
+$extension = pathinfo($moved, PATHINFO_EXTENSION);
+
+$basename = preg_replace('/[A-z-0-9]/', '-', $basename);
+$filepath = 'uploads/' . $basename . '.' . $extension;
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($_FILES['image']['error'] === 0) {
+        $i = 1;
+        $filename = '';
+    
 
         $temp = $_FILES['image']['tmp_name'];
         $message = '<b>File:</b> ' . $_FILES['image']['name'] . '<br>';
         $message .= '<b>File:</b> ' . $_FILES['image']['size'] . '<br>';
         $path = 'uploads/' . $_FILES['image']['name'];
         $moved = move_uploaded_file($temp, $path);
+        while (file_exists('uploads/' . $filename)) {
+            $i = $i + 1;
+            $filename = $basename . $i . '.' . $extension;
+        }
         if ($moved === true) {
             $message = 'img src="' . $path . '">';
         } else {
@@ -39,3 +51,6 @@ $destination = '../uploads/' . $_FILES['image']['name'];
 <?= $_FILES['image']['size']. '<br>' ;?>
 <?= $_FILES['image']['type']. '<br>'; ?>
 <?= $_FILES['image']['error']. '<br>'; ?>
+
+<?= $basename ?>;
+<?= $extension ?>;
---
+++ validating_file_uploads.php
@@ -0,0 +1,13 @@
+<?php
+
+$moved = false;
+$message = '';
+$error = '';
+$upload_path = 'uploads/';
+$max_size = 5242880;
+$allowed_types = ['image/jpeg', 'image/png', 'image/gif',];
+$allowed_exts = ['jpeg', 'jpg', 'png', 'gif',];
+
+
+
+?>
