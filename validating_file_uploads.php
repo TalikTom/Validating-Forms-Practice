2e46282dfc8721583513d2bbc8f4d25fc93478eb
Luka Agic <lukagic@gmail.com>
2023-01-04 19:16:18 +0100

    Create function "create_filename"

--- validating_file_uploads.php
+++ validating_file_uploads.php
@@ -8,6 +8,18 @@ $max_size = 5242880;
 $allowed_types = ['image/jpeg', 'image/png', 'image/gif',];
 $allowed_exts = ['jpeg', 'jpg', 'png', 'gif',];
 
+function create_filename($filename, $upload_path) 
+{
+    $basename = pathinfo($filename, PATHINFO_FILENAME);
+    $extension = pathinfo($filename, PATHINFO_EXTENSION);
+    $basename = preg_replace('/[^A-z0-9]', '-', $basename);
+    $i = 0;
+    while(file_exists($upload_path . $filename)) {
+        $i = $i + 1;
+        $filename = $basename . $i . '.' . $extension;
+    }
 
+    return $filename;
+}
 
 ?>
